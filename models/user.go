84f7303bd65822768ab45678d82e221a5148abea
Diah Novianti <diahnovianti0711@gmail.com>
2023-06-03 22:58:44 +0700

    change some file

--- models/user.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type Users struct {
-	ID       int    `gorm:"primaryKey autoIncrement" json:"id"`
-	UserName string `json:"username"`
-	Name     string `json:"firstName"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
-}
